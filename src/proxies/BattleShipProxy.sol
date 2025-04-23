e1c61831b11dff0224edd474e04f1e6d8f0a2dcf
Njoku Emmanuel <kalunjoku123@gmail.com>
2025-04-23 11:27:09 +0200

    Rename GameImplementation to BattleshipGameImplementation and add proxy contract

---
+++ src/proxies/BattleShipProxy.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+/**
+ * @title BattleshipGameProxy
+ * @dev Proxy contract that delegates calls to the current implementation
+ * @notice This proxy uses ERC1967 standard for upgradeable contracts
+ */
+contract BattleshipGameProxy is ERC1967Proxy {
+    /**
+     * @notice Constructor for the proxy
+     * @param _logic Address of the initial implementation
+     * @param _data Initialization calldata to be passed to implementation
+     */
+    constructor(address _logic, bytes memory _data) ERC1967Proxy(_logic, _data) { }
+}
